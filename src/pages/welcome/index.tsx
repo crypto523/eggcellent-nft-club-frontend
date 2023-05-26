91fcbd9ea3e422e31cd1ca03fecc51bd4883684f
crypto523 <jacksoncurry46@gmail.com>
2023-05-26 08:18:34 +0800

    feat: welcome page completed

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,6 +1,168 @@
-import React from "react";
-import { WelcomePageWrapper } from "./styles";
+import React, { useEffect, useState } from "react";
+import {
+  ChickImgWrapper,
+  RectImgWrapper,
+  WelcomeButtonWrapper,
+  WelcomePageWrapper,
+  WelcomeTitleWrapper,
+} from "./styles";
+
+const WelcomeText: React.FC = () => (
+  <WelcomeTitleWrapper>
+    <video
+      autoPlay
+      playsInline
+      muted
+      loop
+      preload=""
+      poster="/_nuxt/img/rainbow-circle-black.25ba983.jpg"
+    >
+      <source src="/assets/rainbow.mp4" />
+    </video>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid slice"
+    >
+      <defs>
+        <mask id="mask" x="0" y="0" width="100%" height="100%">
+          <rect x="0" y="0" width="100%" height="100%" />
+          <text y={60}>EGGCELENT NFT CLUB</text>
+        </mask>
+      </defs>
+      <rect x="0" y="0" width="100%" height="100%" />
+    </svg>
+    <h1 id="video-mask">EGGCELENT NFT CLUB</h1>
+  </WelcomeTitleWrapper>
+);
 
 export const Welcome: React.FC = () => {
-  return <WelcomePageWrapper>Welcome: React.FC</WelcomePageWrapper>;
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+  const [chickenPos, setChickenPos] =
+    useState<Array<{ x: number; y: number }>>();
+  const [rectPos, setRectPos] = useState<Array<{ x: number; y: number }>>();
+  useEffect(() => {
+    let tempChickPos: Array<{ x: number; y: number }> = [];
+    let tempRectPos: Array<{ x: number; y: number }> = [];
+    for (let i = 0; i < 4; i++) {
+      const chicken = document.getElementById(`chick-${i + 1}`);
+      tempChickPos.push({
+        x: chicken?.getBoundingClientRect().x || 0,
+        y: chicken?.getBoundingClientRect().y || 0,
+      });
+    }
+
+    for (let i = 0; i < 4; i++) {
+      const rect = document.getElementById(`rect-${i + 1}`);
+      tempRectPos.push({
+        x: rect?.getBoundingClientRect().x || 0,
+        y: rect?.getBoundingClientRect().y || 0,
+      });
+    }
+
+    setChickenPos(tempChickPos);
+    setRectPos(tempRectPos);
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  const handleMouseMove: { (e: MouseEvent): void } = (e: MouseEvent) => {
+    setMousePos({ x: e.clientX, y: e.clientY });
+  };
+
+  return (
+    <WelcomePageWrapper>
+      <WelcomeText />
+      <WelcomeButtonWrapper>
+        <button>Let's Explore</button>
+        <img src="/assets/images/welcome.png" alt="" />
+      </WelcomeButtonWrapper>
+      <ChickImgWrapper
+        id="chick-1"
+        pos={mousePos}
+        selfPos={chickenPos ? chickenPos[0] : { x: 0, y: 0 }}
+      >
+        <img src="/assets/images/chick1.svg" alt="" />
+      </ChickImgWrapper>
+      <ChickImgWrapper
+        id="chick-2"
+        pos={mousePos}
+        selfPos={chickenPos ? chickenPos[1] : { x: 0, y: 0 }}
+      >
+        <img src="/assets/images/chick2.svg" alt="" />
+      </ChickImgWrapper>
+      <ChickImgWrapper
+        id="chick-3"
+        pos={mousePos}
+        selfPos={chickenPos ? chickenPos[2] : { x: 0, y: 0 }}
+      >
+        <img src="/assets/images/chick3.svg" alt="" />
+      </ChickImgWrapper>
+      <ChickImgWrapper
+        id="chick-4"
+        pos={mousePos}
+        selfPos={chickenPos ? chickenPos[3] : { x: 0, y: 0 }}
+      >
+        <img src="/assets/images/chick4.svg" alt="" />
+      </ChickImgWrapper>
+      <RectImgWrapper
+        id="rect-1"
+        pos={mousePos}
+        selfPos={rectPos ? rectPos[0] : { x: 0, y: 0 }}
+      >
+        <img src="/assets/images/rect1.png" alt="" />
+      </RectImgWrapper>
+      <RectImgWrapper
+        id="rect-2"
+        pos={mousePos}
+        selfPos={rectPos ? rectPos[0] : { x: 0, y: 0 }}
+      >
+        <img src="/assets/images/rect2.png" alt="" />
+      </RectImgWrapper>
+      <RectImgWrapper
+        id="rect-3"
+        pos={mousePos}
+        selfPos={rectPos ? rectPos[0] : { x: 0, y: 0 }}
+      >
+        <img src="/assets/images/rect3.png" alt="" />
+      </RectImgWrapper>
+      <RectImgWrapper
+        id="rect-4"
+        pos={mousePos}
+        selfPos={rectPos ? rectPos[0] : { x: 0, y: 0 }}
+      >
+        <img src="/assets/images/rect4.png" alt="" />
+      </RectImgWrapper>
+      <RectImgWrapper
+        id="rect-5"
+        pos={mousePos}
+        selfPos={rectPos ? rectPos[0] : { x: 0, y: 0 }}
+      >
+        <img src="/assets/images/rect5.png" alt="" />
+      </RectImgWrapper>
+      <RectImgWrapper
+        id="rect-6"
+        pos={mousePos}
+        selfPos={rectPos ? rectPos[0] : { x: 0, y: 0 }}
+      >
+        <img src="/assets/images/rect6.png" alt="" />
+      </RectImgWrapper>
+      <RectImgWrapper
+        id="rect-7"
+        pos={mousePos}
+        selfPos={rectPos ? rectPos[0] : { x: 0, y: 0 }}
+      >
+        <img src="/assets/images/rect7.png" alt="" />
+      </RectImgWrapper>
+      <RectImgWrapper
+        id="rect-8"
+        pos={mousePos}
+        selfPos={rectPos ? rectPos[0] : { x: 0, y: 0 }}
+      >
+        <img src="/assets/images/rect8.png" alt="" />
+      </RectImgWrapper>
+    </WelcomePageWrapper>
+  );
 };
--- src/pages/welcome/styles.ts
+++ src/pages/welcome/styles.ts
@@ -1,3 +1,249 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-export const WelcomePageWrapper = styled.div``;
+const anim = keyframes`
+  0% {
+    transform: translateY(0px);
+  }
+  50% {
+    transform: translateY(15px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+`;
+
+export const WelcomePageWrapper = styled.div`
+  background-color: #fff2f2;
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const WelcomeTitleWrapper = styled.div`
+  position: relative;
+  z-index: 2;
+  width: 100%;
+  max-width: 800px;
+  height: fit-content;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  h1 {
+    color: #00000000;
+    font-size: 50px;
+  }
+  video {
+    position: absolute;
+    z-index: 1;
+    width: 100%;
+    object-fit: cover;
+  }
+
+  svg {
+    width: 100%;
+    position: absolute;
+    z-index: 2;
+    top: 0;
+    left: 0; // needed for FF, Safari, Edge
+    height: 100%;
+    text {
+      font-family: filson-soft;
+      font-weight: 900;
+      text-transform: uppercase;
+      width: 100%;
+      font-size: 72px;
+    }
+    rect {
+      fill: #fff7f7;
+    }
+    > rect {
+      -webkit-mask: url(#mask);
+      mask: url(#mask);
+    }
+  }
+  @media screen and (max-width: 768px) {
+    max-width: 650px;
+    svg {
+      text {
+        font-size: 60px;
+      }
+    }
+  }
+  @media screen and (max-width: 650px) {
+    max-width: 345px;
+    svg {
+      text {
+        font-size: 32px;
+      }
+    }
+    h1 {
+      font-size: 40px;
+    }
+  }
+`;
+
+export const WelcomeButtonWrapper = styled.div`
+  margin-top: 40px;
+  position: relative;
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  button {
+    cursor: pointer;
+    text-transform: uppercase;
+    font-size: 24px;
+    font-family: filson-soft;
+    width: 248px;
+    color: #fff;
+    font-weight: 800;
+    background-color: #8459ff;
+    border: none;
+    outline: none;
+    border-radius: 68px;
+    height: 68px;
+  }
+  img {
+    width: 250px;
+    margin-top: 20px;
+  }
+`;
+
+export const RectImgWrapper = styled.div<{
+  pos: { x: number; y: number };
+  selfPos: { x: number; y: number };
+}>`
+  position: absolute;
+  transition: all 0.3s ease-in-out;
+  &#rect-1 {
+    img {
+      animation: ${anim} 2.5s ease-in-out infinite;
+      width: 290px;
+    }
+    top: -100px;
+    left: 45%;
+    transform: ${({ pos, selfPos }) =>
+      `translateX(${(pos.x - selfPos.x) / 15}px) translateY(${
+        (pos.y - selfPos.y) / 20
+      }px)`};
+  }
+  &#rect-2 {
+    img {
+      animation: ${anim} 3s ease-in-out infinite;
+      width: 250px;
+    }
+    top: 100px;
+    left: 70px;
+    transform: ${({ pos, selfPos }) =>
+      `translateX(${(pos.x - selfPos.x) / 17}px) translateY(${
+        (pos.y - selfPos.y) / 17
+      }px)`};
+  }
+  &#rect-3 {
+    img {
+      animation: ${anim} 2.7s ease-in-out infinite;
+      width: 400px;
+    }
+    bottom: -100px;
+    left: -100px;
+    transform: ${({ pos, selfPos }) =>
+      `translateX(${(pos.x - selfPos.x) / 25}px) translateY(${
+        (pos.y - selfPos.y) / 30
+      }px)`};
+  }
+  &#rect-4 {
+    img {
+      animation: ${anim} 3.1s ease-in-out infinite;
+      width: 500px;
+    }
+    right: -100px;
+    bottom: -100px;
+    transform: ${({ pos, selfPos }) =>
+      `translateX(${(pos.x - selfPos.x) / 21}px) translateY(${
+        (pos.y - selfPos.y) / 15
+      }px)`};
+  }
+  &#rect-5 {
+    img {
+      animation: ${anim} 2.5s ease-in-out infinite;
+      width: 250px;
+    }
+    right: 150px;
+    bottom: 350px;
+    transform: ${({ pos, selfPos }) =>
+      `translateX(${(pos.x - selfPos.x) / 26}px) translateY(${
+        (pos.y - selfPos.y) / 30
+      }px)`};
+  }
+  &#rect-6 {
+    img {
+      animation: ${anim} 2.5s ease-in-out infinite;
+      width: 200px;
+    }
+    left: 400px;
+    bottom: 250px;
+    transform: ${({ pos, selfPos }) =>
+      `translateX(${(pos.x - selfPos.x) / 30}px) translateY(${
+        (pos.y - selfPos.y) / 20
+      }px)`};
+  }
+`;
+
+export const ChickImgWrapper = styled.div<{
+  pos: { x: number; y: number };
+  selfPos: { x: number; y: number };
+}>`
+  position: absolute;
+  z-index: 1;
+  transition: all 0.3s ease-in-out;
+  &#chick-1 {
+    img {
+      animation: ${anim} 2.5s ease-in-out infinite;
+    }
+    top: 130px;
+    left: 270px;
+    transform: ${({ pos, selfPos }) =>
+      `translateX(${(pos.x - selfPos.x) / 30}px) translateY(${
+        (pos.y - selfPos.y) / 30
+      }px)`};
+  }
+  &#chick-2 {
+    img {
+      animation: ${anim} 2.8s ease-in-out infinite;
+    }
+    top: 100px;
+    right: 200px;
+    transform: ${({ pos, selfPos }) =>
+      `translateX(${(pos.x - selfPos.x) / 22}px) translateY(${
+        (pos.y - selfPos.y) / 22
+      }px)`};
+  }
+  &#chick-3 {
+    img {
+      animation: ${anim} 2.3s ease-in-out infinite;
+    }
+    bottom: 100px;
+    left: 240px;
+    transform: ${({ pos, selfPos }) =>
+      `translateX(${(pos.x - selfPos.x) / 23}px) translateY(${
+        (pos.y - selfPos.y) / 23
+      }px)`};
+  }
+  &#chick-4 {
+    img {
+      animation: ${anim} 3s ease-in-out infinite;
+    }
+    bottom: 100px;
+    right: 200px;
+    transform: ${({ pos, selfPos }) =>
+      `translateX(${(pos.x - selfPos.x) / 25}px) translateY(${
+        (pos.y - selfPos.y) / 25
+      }px)`};
+  }
+`;
