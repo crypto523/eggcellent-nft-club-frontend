f52a4ac0e45855063e1bfa90888ff93fa22b32d3
crypto523 <jacksoncurry46@gmail.com>
2023-05-26 21:56:14 +0800

    feat: banner image and title animation

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { LandingPageWrapper } from "./styles";
 import { AppLayout } from "../../layouts/AppLayout";
 import {
@@ -9,9 +9,63 @@ import {
 } from "../../modules";
 
 export const Landing: React.FC = () => {
+  const title1 = "EGGCELLENT";
+  const title2 = "NFT";
+  const title3 = "CLUB";
+  const title4 = "Chicken";
+  const title5 = "Eggs";
+  const title6 = "Farming";
+  const [anim, setAnim] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    const titleElem = document
+      .getElementById("home-title")
+      ?.getBoundingClientRect().top;
+    if (window.innerHeight >= Number(titleElem) && Number(titleElem) >= 0) {
+      setAnim(true);
+    } else {
+      setAnim(false);
+    }
+  };
+
   return (
     <AppLayout>
-      <LandingPageWrapper id="home" />
+      <LandingPageWrapper id="home">
+        <h1 id="home-title" className={anim ? "anim" : ""}>
+          {title1.split("").map((item, key) => (
+            <b key={key}>{item}</b>
+          ))}
+          &nbsp;
+          {title2.split("").map((item, key) => (
+            <b key={key}>{item}</b>
+          ))}
+          &nbsp;
+          {title3.split("").map((item, key) => (
+            <b key={key}>{item}</b>
+          ))}
+          <br />
+          <span>
+            {title4.split("").map((item, key) => (
+              <b key={key}>{item}</b>
+            ))}
+            &nbsp;
+            {title5.split("").map((item, key) => (
+              <b key={key}>{item}</b>
+            ))}
+            &nbsp;
+            {title6.split("").map((item, key) => (
+              <b key={key}>{item}</b>
+            ))}
+          </span>
+        </h1>
+      </LandingPageWrapper>
       <GitBookSection />
       <RoadMapSection />
       <TeamSection />
--- src/pages/landing/styles.ts
+++ src/pages/landing/styles.ts
@@ -1,11 +1,37 @@
 import styled from "styled-components";
 
 export const LandingPageWrapper = styled.div`
-  padding-top: 117px;
-  background: url("/assets/images/Eggcellent.jpg") no-repeat;
+  padding-top: 200px;
+  background: url("/assets/images/Eggcellent.png") no-repeat;
   background-size: cover;
   background-position: top center;
   min-height: 100vh;
+  h1 {
+    text-align: center;
+    color: #240d13;
+    &.anim {
+      b {
+        animation: anim 2s ease-in-out;
+      }
+    }
+    b {
+      font-family: "Anton", sans-serif !important;
+      display: inline-block;
+    }
+    font-weight: 400;
+    font-size: 70px;
+    line-height: 100%;
+    letter-spacing: 0.04em;
+    span {
+      font-size: 60px;
+      b {
+        font-family: filson-soft !important;
+        font-weight: 100;
+      }
+      letter-spacing: 0;
+      font-weight: 100;
+    }
+  }
   @media screen and (max-width: 768px) {
     padding-top: 75px;
   }
